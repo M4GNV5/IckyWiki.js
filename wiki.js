1c70a6052b7809e0a0a6f39973c163b4a93d4db8
Jakob Löw <eragonjml@googlemail.com>
2016-01-11 14:28:56 +0100

    fixing special chars and removing content type

--- wiki.js
+++ wiki.js
@@ -1,7 +1,7 @@
-r=require,f="./p.json",p=r(f),e="replace";r("http").createServer((q,a)=>{a.
-writeHead(200,{"Content-Type":"text/html"});u=q.url,m=u[1]!="e",s=decodeURI(u)[e
-](/\+/g," ").split(/[?\/=]/g),n=s[2],c=p[n]=s[4]||p[n]||"";if(m){c+="\n\nsee "
-for(k in p){if(p[k].indexOf(n)+1)c+=k+" ";c=c[e](RegExp(k,"g"),`<a href="/r/${k
-}">${k}</a>`);}r("fs").writeFile(f,JSON.stringify(p));}a.end(m?`<h1>${n}</h1>${
-c[e](/\n/g,"<br>")}<br><a href="/e/${n}">edit`:`<h1>${n}</h1><form action=/r/${n
-} method=GET><textarea name=s>${c}</textarea><button>ok`);}).listen(1337);
+r=require,f="./p.json",p=r(f),e="replace",l='<a href="$&">$&</a>';r("http").
+createServer((q,a)=>{s=q.url.split(/[\/?]/g),m=s[1]!="e",n=s[2],c=p[n]=r(
+"querystring").parse(s[3]).s||p[n]||"404";if(m){c=c[e](/</g, "&lt;")+"\n\nsee "
+for(k in p){if(p[k].indexOf(n)+1)c+=k+" ";c=c[e](RegExp(k,"g"),l);}r("fs").
+writeFile(f,JSON.stringify(p))}a.end(`<h1>${n}</h1>`+(m?`${c[e](/http^\s+/g,l)[e
+](/\n/g,"<br>")}<br><a href="/e/${n}">edit`:`<form action=/r/${n} method=GET>
+<textarea name=s>${c}</textarea><button>ok`))}).listen(1337);
